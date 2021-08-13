95216eed505edbe7a231692e0460d43831ba49b7
Adam Carter <67630275+AMACarter@users.noreply.github.com>
2021-08-13 18:45:51 +0100

    flamingo foundation code added

--- bot/flamingo-0.0.1.py
+++ bot/flamingo-0.0.1.py
@@ -1,1 +1,30 @@
 import alpaca_trade_api as tradeapi
+from alpaca_trade_api.rest import REST
+import logging
+
+if __name__ == '__main__':
+
+api = tradeapi.REST('PKPVWUKI5H0UBVALQUXM','QQrNRzBk5RTfVoRX8ISdYrWeQwHh51XmDb6LVSBh','https://paper-api.alpaca.markets/v2/account')
+
+# Get our account information.
+account = api.get_account()
+
+# Check if our account is restricted from trading.
+if account.trading_blocked:
+    print('Account is currently restricted from trading.')
+
+# Check how much money we can use to open new positions.
+    print('${} is available as buying power.'.format(account.buying_power))
+
+# Get a list of all of our positions.
+portfolio = api.list_positions()
+
+# Print the quantity of shares for each position.
+for position in portfolio:
+    print("{} shares of {}".format(position.qty, position.symbol))
+
+# Check our current balance vs. our balance at the last market close
+balance_change = float(account.equity) - float(account.last_equity)
+    print(f'Today\'s portfolio balance change: ${balance_change}')
+
+
