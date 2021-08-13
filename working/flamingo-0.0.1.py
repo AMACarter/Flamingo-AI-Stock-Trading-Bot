0dfe54a86120dd55924ec2e9e80479a406f50b0f
Adam Carter <67630275+AMACarter@users.noreply.github.com>
2021-08-13 20:15:48 +0100

    added logging and threads

--- working/flamingo-0.0.1.py
+++ working/flamingo-0.0.1.py
@@ -1,5 +1,7 @@
 import alpaca_trade_api as tradeapi # Alpaca API
 import sys 
+import datetime
+import time
 import logging # Data logging
 import scipy # Linear algebra
 import numpy # Arrays and Matrices
@@ -7,8 +9,10 @@ import matplotlib # Plotting
 import pandas # Data manipulation
 import sklearn # Machine learning
 import tkinter as tk # GUI elements
+import threading # Thread management
 
 from alpaca_trade_api.rest import REST
+from alpaca_trade_api.rest import TimeFrame
 from matplotlib import pyplot
 from pandas import read_csv
 
@@ -49,3 +53,7 @@ for position in portfolio:
 # Check our current balance vs. our balance at the last market close
 balance_change = float(account.equity) - float(account.last_equity)
 print(f'Today\'s portfolio balance change: ${balance_change}')
+
+# Logging different components
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
