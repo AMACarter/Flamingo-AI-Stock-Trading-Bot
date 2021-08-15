49507f732102bac749090262f996b05de8103241
Adam Carter <67630275+AMACarter@users.noreply.github.com>
2021-08-15 20:50:09 +0100

    changed threading to multiprocessing

--- working/flamingo-0.0.1.py
+++ working/flamingo-0.0.1.py
@@ -1,7 +1,9 @@
+from re import T
 import alpaca_trade_api as tradeapi # Alpaca API
 import sys 
 import datetime
 import time
+import threading
 import logging # Data logging
 import scipy # Linear algebra
 import numpy # Arrays and Matrices
@@ -9,51 +11,71 @@ import matplotlib # Plotting
 import pandas # Data manipulation
 import sklearn # Machine learning
 import tkinter as tk # GUI elements
-import threading # Thread management
+import multiprocessing # Thread management
+
 
 from alpaca_trade_api.rest import REST
 from alpaca_trade_api.rest import TimeFrame
 from matplotlib import pyplot
 from pandas import read_csv
+from multiprocessing import Process
+     
+# LAUNCH GUI
 
-# Launch application & GUI
-# window = tk.Tk()
-# window.title("Flamingo Trading Bot")
+def flamingo_gui():
+    
+    window = tk.Tk()
+    window.title("Flamingo Trading Bot")
 
-# label = tk.Label(text="")
-# label.pack()
+    label = tk.Label(text="")
+    label.pack()
+    window.mainloop() 
 
-# window.mainloop()
+# CONNECTION TO API AND ACCOUNT
 
-if __name__ == '__main__':
-
+def flamingo_startup():
+    
     api = tradeapi.REST(
         'PKPVWUKI5H0UBVALQUXM',
         'QQrNRzBk5RTfVoRX8ISdYrWeQwHh51XmDb6LVSBh',
         'https://paper-api.alpaca.markets', api_version='v2'
     )
+    # Get our account information.
+    account = api.get_account()
 
-# Get our account information.
-account = api.get_account()
+    # Check if our account is restricted from trading.
+    if account.trading_blocked:
+        print('Account is currently restricted from trading.')
 
-# Check if our account is restricted from trading.
-if account.trading_blocked:
-    print('Account is currently restricted from trading.')
+    # Check how much money we can use to open new positions.
+        print('${} is available as buying power.'.format(account.buying_power))
 
-# Check how much money we can use to open new positions.
-    print('${} is available as buying power.'.format(account.buying_power))
+    # Get a list of all of our positions.
+    portfolio = api.list_positions()
 
-# Get a list of all of our positions.
-portfolio = api.list_positions()
-
-# Print the quantity of shares for each position.
-for position in portfolio:
-    print("{} shares of {}".format(position.qty, position.symbol))
+    # Print the quantity of shares for each position.
+    for position in portfolio:
+        print("{} shares of {}".format(position.qty, position.symbol))
     
-# Check our current balance vs. our balance at the last market close
-balance_change = float(account.equity) - float(account.last_equity)
-print(f'Today\'s portfolio balance change: ${balance_change}')
+    # Check our current balance vs. our balance at the last market close
+    balance_change = float(account.equity) - float(account.last_equity)
+    print(f'Today\'s portfolio balance change: ${balance_change}')
 
-# Logging different components
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+    # Logging different components
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+# MAIN THREAD
+
+if __name__ == '__main__': 
+    
+    p1 = multiprocessing.Process(target=flamingo_startup)
+    p2 = multiprocessing.Process(target=flamingo_gui)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    print("Flamingo Bot Initialised.")
 
