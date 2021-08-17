01e1bbc22dc137f4500861a1506fb35846588698
Adam Carter <67630275+AMACarter@users.noreply.github.com>
2021-08-17 15:49:32 +0100

    added working TKINKER framework and portfolio updater

--- working/flamingo.py
+++ working/flamingo.py
@@ -7,6 +7,7 @@ import importlib
 import threading
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import logging # Data logging
 import tkinter as tk # GUI elements
 import multiprocessing # Thread management
@@ -21,37 +22,18 @@ from re import T
 from tkinter import *
 from colorama import Fore, Back, Style
 from functools import partial
-  
-# LAUNCH GUI
-
-def flamingo_gui():
-    
-    window = tk.Tk()
-    window.title("Flamingo Trading Bot")
-    window.geometry('400x150')  
-        
-    # Get users API keys
-    
-    # UserApiKeyLabel = tk.Label(window, text="API Key").grid(row=0, column=0)
-    # UserApiKey = StringVar()
-    # UserApiEntry = Entry(window, textvariable=UserApiKey).grid(row=0, column=1)
-    
-    # UserSecretKeyLabel = tk.Label(window, text="Secret key").grid(row=1, column=0)
-    # UserSecretKey = StringVar()
-    # UserSecretKeyEntry = Entry(window, textvariable=UserSecretKey).grid(row=1, column=1)
-    
-    # validateAPI = partial(validateAPI, UserApiKey, UserSecretKey)
-    
-    # AuthButton =Button(window, text="Validate", command=validateAPI).grid(row=3, column=1)
-      
-    window.mainloop() 
-
-
+from datetime import datetime
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
 
 # CONNECTION TO API AND ACCOUNT
 
+global api
+
 def flamingo_startup():
-    
+   
     # Setup Alpaca API 
     api_key = 'PKPVWUKI5H0UBVALQUXM'
     api_secret = 'QQrNRzBk5RTfVoRX8ISdYrWeQwHh51XmDb6LVSBh'
@@ -115,28 +97,69 @@ def flamingo_startup():
     
     # Logging different components
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
-    
-    # Portfolio Plotting
-    histpd={}
-    for pd in ['1D', '2D', '3D', '7D']:
-        hist = api.get_portfolio_history(
-            period=pd,
-            extended_hours=True,
-            timeframe='1Min'
-        ).df
-        histpd[pd] = hist
-    # print(histpd)
-      
-    plt.plot(histpd)
-    plt.xlabel('Date (day(s))')
-    plt.ylabel('Portfolio Value ($)')
-    plt.show()
-    
+       
     print (Fore.GREEN + "Flamingo Bot Initialised.")
     print (Style.RESET_ALL)
     
     # Open broker dashboard
     # webbrowser.open('https://app.alpaca.markets/brokerage/dashboard/overview', new=2, autoraise=True)
+
+# LAUNCH GUI
+
+def flamingo_gui():
+    
+    # Setup Alpaca API 
+    api_key = 'PKPVWUKI5H0UBVALQUXM'
+    api_secret = 'QQrNRzBk5RTfVoRX8ISdYrWeQwHh51XmDb6LVSBh'
+    base_url = 'https://paper-api.alpaca.markets'
+    data_url = 'wss://data.alpaca.markets'
+    
+    # Initiate REST API
+    api = tradeapi.REST(
+    api_key, 
+    api_secret, 
+    base_url, 
+    api_version='v2')
+    
+    histtf={}
+    for tf in ['15Min']:
+        hist = api.get_portfolio_history(
+            date_start='2021-08-15',
+            period="7D",
+            extended_hours=True,
+            timeframe=tf
+        ).df
+        histtf[tf] = hist
+    
+    root = Tk()
+    root.title("Flamingo Trading Bot")
+    root.geometry("1920x1080")
+    
+    fig, (ax1,ax2) = plt.subplots(2)
+    fig.suptitle('Portfolio')
+    ax1.plot(hist.equity)
+    ax2.plot(hist.profit_loss)
+    
+    canvas = FigureCanvasTkAgg(fig, root)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+        
+    # Get users API keys
+    
+    # UserApiKeyLabel = tk.Label(window, text="API Key").grid(row=0, column=0)
+    # UserApiKey = StringVar()
+    # UserApiEntry = Entry(window, textvariable=UserApiKey).grid(row=0, column=1)
+    
+    # UserSecretKeyLabel = tk.Label(window, text="Secret key").grid(row=1, column=0)
+    # UserSecretKey = StringVar()
+    # UserSecretKeyEntry = Entry(window, textvariable=UserSecretKey).grid(row=1, column=1)
+    
+    # validateAPI = partial(validateAPI, UserApiKey, UserSecretKey)
+    
+    # AuthButton =Button(window, text="Validate", command=validateAPI).grid(row=3, column=1)
+      
+    root.mainloop() 
+
 
 # MAIN THREAD
 
